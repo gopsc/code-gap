afa10f2a0778401c711037eb4f915cd6fdfea436
gopsc <35726593+gopsc@users.noreply.github.com>
2018-06-05 20:39:23 -0500

    we don't delete old code,:) and  again

--- seed/network/message/data/update/append.h
+++ seed/network/message/data/update/append.h
@@ -42,6 +42,8 @@ int append_message_data_update(
 //
 // this is not update time
 //
+//  and we don need to use it
+//
 //  and if the client have not return
 //
 //   we don't read, and keep this
@@ -103,6 +105,17 @@ int append_message_data_update(
             strcpy(buffer_path_start, path_the);
             strcat(buffer_path_start, "/"     );
             strcat(buffer_path_start, name_the);
+
+
+
+
+
+/* if ( strcmp(gop_connection.update[that_site], buffer_path_start) == 0 ) {// the loop program returned wrong flag//// if this is just like started just before//// there're two kind of possibility//// it is a beginning, something goes wrong// or it's end////// but we read the returned before//// so if it's like a beginning now//// it must be a beginning//// because we we will clean the buffer when the if ( control_message_update(gop_connection.update[that_site]) == 0 ) {// so we just loop it first//// if it returned a special value that means something really goes wrong//// it is done, we should never send a update message and we can fix it output_print( "string", "There is no TxL source.\n" );// clean it. strcpy(gop_connection.update[that_site], "" );// clean buffer of send *strstr(buffer_send, "Update.") = '\0';// this is a wrong,,, return 0;}}// ok we had loop it before in the judging's value//// now we get the files file_get( gop_connection.update[that_site], buffer_file );// and get the length of the file snprintf( buffer_file_length, sizeof(buffer_file_length) / sizeof(char) - 1, "%d", strlen(buffer_file) );// some file is very long, we can't send it once//// even if we can't read it unless used the changed buffer size//// and i still can't use that :c if ( gop_connection.step_update[that_site] * size_update > strlen(buffer_file) ) {// the value named 'step_update' means how many times we have send it//// and if this is over the last time of send this file already,//// we should loop it again//// for next append// loop it again if ( control_message_update(gop_connection.update[that_site]) != 0 ) {// this is the last step, but not the last file//// and be ready to send next file; gop_connection.step_update[that_site] = 0;} else {// if it is normal before//// and returned wrong now//// it means there is no another one file now// clean it and send message strcpy(gop_connection.update [that_site], "" ); gop_connection.step_update[that_site] = 0;
+
+
+
+
+
 
 
 
