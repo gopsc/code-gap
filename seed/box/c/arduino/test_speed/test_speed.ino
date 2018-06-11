66dcc8d9f5fe87d52eb47cea4a9acbec03ac90d4
gopsc <35726593+gopsc@users.noreply.github.com>
2018-06-11 19:40:05 +0800

    god they're so quick
    it's ok to corde 8000+ times in one seconds

---
+++ seed/box/c/arduino/test_speed/test_speed.ino
@@ -0,0 +1,32 @@
+
+    int  number_count = 0;
+    int  number_limit = 100;
+    long number_time  = 0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+    Serial.begin(9600);
+
+    number_limit = 100;}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  number_time  = millis();
+
+  
+  for ( int i=1; i<8000; i++ ) {
+
+      analogRead ( A0 );
+      
+      if  (  0 > number_limit  ) {
+        
+        delay(0);}}
+
+  Serial.println( millis() - number_time );
+}
