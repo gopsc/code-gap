07610efb1398b147a2894ca203bfc4ea58edaf94
gopsc <35726593+gopsc@users.noreply.github.com>
2018-06-13 23:17:06 +0800

    yes it it fast don't the wave force how to

--- seed/box/c/arduino/test_speed/test_speed.ino
+++ seed/box/c/arduino/test_speed/test_speed.ino
@@ -1,4 +1,15 @@
 
+
+
+/*
+
+ test the speed of analog regist
+ 
+ 870000+ for uno
+ 345567+ for gr-citrus
+
+*/
+
     long number_time  = 0;
 
 
@@ -17,12 +28,12 @@ void loop() {
     
   // put your main code here, to run repeatedly:
 
-  number_time  = millis();
+  number_time  = micros();
 
   
   for ( int i=1; i<8000; i++ ) {
 
       analogRead ( A0 );}
 
-  Serial.println( millis() - number_time );
+  Serial.println( micros() - number_time );
 }
