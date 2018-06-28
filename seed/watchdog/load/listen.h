f52d6c5aabcd7057fddb3783c8d81abcf470323d
gopsc <1776177662@qq.com>
2018-06-28 13:01:20 +0100

    a little

--- seed/watchdog/load/listen.h
+++ seed/watchdog/load/listen.h
@@ -8,10 +8,13 @@ int load_listen() {
 
 
 
+/*
 
-// the path of the files saved
+ the path of the files saved
 
-// now it is trees/*
+ it is  trees/*  now
+
+*/
 
     char buffer_path [ 128 ];
 
@@ -20,24 +23,29 @@ int load_listen() {
 
 // It's for saving the name of the dirent
 
-    char buffer_name[128];
+    char buffer_name [ 128 ];
 
 
 // it's for saving the name of the files
 
-    char buffer_file[128];
+    char buffer_file [ 128 ];
 
 
-// it's for saving the file content
+/*
 
-    char buffer_word[10240];
+ it's for saving the file content
+
+ soon we use realloc
+
+*/
+    char buffer_word [ 10240 ];
 
 
 // Prepare the path
 
-    strcpy(buffer_path, path_the   );
-    strcat(buffer_path, "/"        );
-    strcat(buffer_path, name_data  );
+    strcpy ( buffer_path, path_the  );
+    strcat ( buffer_path, "/"       );
+    strcat ( buffer_path, name_data );
 
 
 
@@ -48,12 +56,12 @@ int load_listen() {
 
 // if is for reading the file convent
 
-    char* pointer_word;
+    char * pointer_word;
 
 
 // if is for reading the file entre
 
-    char* pointer_enter;
+    char * pointer_enter;
 
 
 
@@ -67,70 +75,122 @@ int load_listen() {
 
     do {
 
+/*
 
-// changed the number of listen 
-//     and into a buffer
+ changed the number of listen 
+     and into a buffer
 
-        snprintf(buffer_word, sizeof(buffer_word) / sizeof(char) - 1, "%d", number_listen);
+*/
 
-
-// Prepare the path
-//
-// TxL/trees/number/w/t.snake
-//                  .
-
-        strcpy(buffer_name, buffer_path);
-        strcat(buffer_name, "/");
-        strcat(buffer_name, buffer_word);
-
-        number_listen++;
+        snprintf  (
+                     buffer_word,
+                     sizeof ( buffer_word ) / sizeof ( char ) - 1,
+                     "%d",
+                     number_listen
+                  );
 
 
 
-// If this dirent is exist
-// we read it
+
+/*
+
+ Prepare the path
+
+ TxL/trees/number/w/t.snake
+
+*/
+
+        strcpy ( buffer_name, buffer_path );
+        strcat ( buffer_name, "/"         );
+        strcat ( buffer_name, buffer_word );
+
+
+
+
+
+/*
+
+ set it one
+
+*/
+
+        number_listen ++;
+
+
+/*
+
+ If this dirent is exist
+ we read it
+
+*/
 
         if ( is_dirent(buffer_name) ) {
 
 
 // Print it
-            output_print( "string",  buffer_name );
-            output_print( "string", "\n"         );
+
+            output_print ( "string",  buffer_name );
+            output_print ( "string", "\n"         );
 
 
 
-// Take a loop to read note  files
-//
-//...
-//
-// Take a loop to read snake files
+
+
+/*
+
+ Take a loop to read note  files
+
+ ...
+
+ Take a loop to read snake files
+
+*/
 
             int w;
 
             for ( w = 1; w <= N; w++ ) {
 
 
-// Find a place to transform
-//  the number of w
+/*
 
-                snprintf(buffer_word, sizeof(buffer_word) / sizeof(char) - 1, "%d", w);
+ Find a place to transform
+  the number of w
 
-// Prepare the file name
-// TxL/trees/number/w/t.snake
-//                    .
+*/
 
-                strcpy(buffer_file, buffer_name  );
-                strcat(buffer_file, "/"          );
-                strcat(buffer_file, buffer_word  );
-                strcat(buffer_file, ".snake"     );
+                snprintf (
+                           buffer_word,
+                           sizeof ( buffer_word )  /  sizeof ( char ) - 1,
+                          "%d",
+                           w
+                         );
+
+
+/*
+
+ Prepare the file name
+ TxL/trees/number/w/t.snake
+                    .
+*/
+
+                strcpy ( buffer_file, buffer_name );
+                strcat ( buffer_file, "/"         );
+                strcat ( buffer_file, buffer_word );
+                strcat ( buffer_file, ".snake"    );
 
 
 
-// if the file does not exist
+/*
+
+ if the file does not exist
+
+ read next onr
+
+*/
 
                      if ( !is_file(buffer_file) ) {
 
-                    break;}
+                    continue;}
 
 
 
