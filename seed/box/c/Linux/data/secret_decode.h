e9c876cb9a1d06ce61501c14e8c97b7a4927735c
gopsc <35726593+gopsc@users.noreply.github.com>
2018-09-02 12:20:10 +0800

    why files are hiddin? o .o

---
+++ seed/box/c/Linux/data/secret_decode.h
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+ int secret_decode (
+                           char * that_message,
+                     const char * that_type
+                   )
+
+  {
+
+
+
+
+
+
+
+
+/*
+
+ use to test the way before
+
+*/
+
+
+
+/*
+
+
+ if  (  strcmp ( that_type, "blank" )  ==  0  )
+
+
+  {
+
+ return 0;
+
+  }
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ don't know ...
+
+*/
+
+
+
+
+
+
+ return -1;
+
+
+
+  }
+
+
+
