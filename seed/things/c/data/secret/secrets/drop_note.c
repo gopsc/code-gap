e442e4b588943132d2dcb51a6538e858817af4ef
gapsc <1776177662@qq.com>
2018-10-05 13:39:38 +0800

    it a little complicate.. :(

--- seed/things/c/data/secret/secrets/drop_note.c
+++ seed/things/c/data/secret/secrets/drop_note.c
@@ -18,9 +18,19 @@
 
 /*
 
- drop note in message 
+ drop note in message
 
- use to read note 
+ use to read note
+
+
+
+ warnning :
+
+ this could just drop note in a string words
+
+ so we just drop things just like ~/seed/seed.note
+
+ but we have ways to finish this problem
 
 
 
@@ -33,6 +43,11 @@
 
  char * note_message;
 
+ char * note_pointer;
+
+ int    note_number;
+
+
  char note_flag [2] [1] [4];
 
  strcpy ( note_flag [0] [0], "//" );
@@ -41,8 +56,8 @@
  strcpy ( note_flag [1] [0], "#"  );
  strcpy ( note_flag [1] [1], "\n" );
 
- strcpy ( note_flag [2] [0], "/*"   );
- strcpy ( note_flag [2] [1], "*/\n" );
+ strcpy ( note_flag [2] [0], "/*" );
+ strcpy ( note_flag [2] [1], "*/" );
 
 
 
@@ -58,7 +73,18 @@
   {
 
 
+
+
  note_message  =  strstr ( that_message, note_flag [i] [0] );
+
+
+/*
+
+ found it
+
+ go to judge and drop
+
+*/
 
  if (
       note_message != NULL
@@ -66,13 +92,60 @@
 
   {
 
- 
+
+
+
+/*
+
+ it could be a note we found
+
+ so find " before it judge by number of it
+
+*/
+
+ * note_message = '\n';
+
+
+
+
+ do
+
+  {
+
+ note_pointer  =  strstr ( that_message, "\"" );
+
+ if ( note_pointer != NULL )
+
+  {
+
+ note_number ++;
+
+  }
+
+  }  while  ( note_pointer != NULL )
+
+
+
+
+ * note_message = note_flag [i] [0] [0];
 
   }
 
 
+
+
   }  while  (  note_message != NULL  )
 
+
+
+
+
+
+/*
+
+ loop for word "for"
+
+*/
 
   }
 
