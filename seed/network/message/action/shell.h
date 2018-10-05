ed425a836d882aba0823de915bc009d6927ba34b
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-05 14:38:31 +0800

    place changed and make it soft

--- seed/network/message/action/shell.h
+++ seed/network/message/action/shell.h
@@ -2,41 +2,62 @@
 
 
 
-int action_shell(
+ int action_shell (
 
                     char* that_command
 
-                ) {
+                  )
+                  
+  {
 
 
 
 
-    char buffer_command[65];
+ char buffer_command [ 65 ];
 
 
 
 
-    if ( strstr(that_command, "Shell ")  != NULL ) {
+ if  (
+       strstr ( that_command, "Shell " )  !=  NULL
+     )
+ 
+  {
 
 
 
 
-        strcpy(buffer_command, "bash ");
+ strcpy ( buffer_command, "bash " );
 
-        strcat(buffer_command, path_the);
+ strcat ( buffer_command, path_the );
 
-        strcat(buffer_command, "/");
+ strcat ( buffer_command, "/" );
 
-        strcat(buffer_command, name_the);
+ strcat ( buffer_command, name_the );
 
-        strcat(buffer_command, "/box/Linux/");
+ strcat ( buffer_command, "/things/shell/" );
 
-        strcat(buffer_command, strstr(that_command, "Shell ") + 6);}
+ strcat (
+          buffer_command,
+          strstr ( that_command, "Shell " ) + 6
+        );
+ 
+ 
+ 
+  }
+
+
+/*
+
+ mabe i should use command get
+
+*/
+   
+ system ( buffer_command );
 
 
 
-    system(buffer_command);
-
-
-
-    return 1;}
+ return 1;
+ 
+ 
+   }
