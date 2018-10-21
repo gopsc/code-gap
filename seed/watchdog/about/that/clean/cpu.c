0aaf297dd59aec9606d2aba0b25e26a06e8e8bc0
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 17:20:46 +0800

    make it cimple
    mv cpu here

---
+++ seed/watchdog/about/that/clean/cpu.c
@@ -0,0 +1,20 @@
+
+ int clean_about_cpu ( gopi_cpu that_cpu )
+ 
+  {
+  
+ 
+
+
+ that_cpu . temperature = 0;
+
+ that_cpu . core = 0;
+
+ that_cpu . frequency = 0;
+
+ that_cpu . rate = 0;
+
+ 
+ return 0
+ 
+  }
