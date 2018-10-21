f98dd031a33c869e302a03163440caf8593ea254
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 16:08:03 +0800

    make it simple

---
+++ seed/watchdog/about/that/that/sound.c
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ struct gopi_sound
+
+  {
+
+ char symbol [ 16 ]
+
+
+ int number_control;
+
+ int number_capture;
+
+ int number_playback;
+
+
+
+/*
+
+ for numbers 128 is too long
+
+*/
+
+ char device [ 3 ] [ 32 ] [ 16 ];
+
+ int device_playback;
+
+ int device_capture;
+
+
+  };
