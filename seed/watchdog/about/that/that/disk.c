3578efbbf90f05c5abb83d117d3750bda617a6cc
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 16:06:38 +0800

    make it simple

---
+++ seed/watchdog/about/that/that/disk.c
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ struct gopi_disk
+
+  {
+
+ char symbol [ 16 ];
+
+
+ unsigned long  size;
+
+ unsigned long  available;
+
+/*
+
+ we note rate straitly
+
+*/
+
+ unsigned long  rate;
+
+  };
