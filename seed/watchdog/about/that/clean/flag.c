29cc4d4079f8b65bf20a985b12610b3f47347563
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 16:21:42 +0800

    make it smaller
    this is flag if it cleanning, it get value to make it smarter

---
+++ seed/watchdog/about/that/clean/flag.c
@@ -0,0 +1,87 @@
+
+
+
+
+
+/*
+
+ then it could clean mates of this
+
+*/
+
+
+ int clean_flag ( gopi_about that_about )
+ 
+  {
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+/*
+
+ first add this before change about name
+
+    or the dog won't run
+
+
+ and dog load configurations
+
+
+
+ sometimes we clean it
+
+ when lost a connectting
+
+*/
+
+
+
+
+
+
+
+/*
+
+ it is running default or program get a shutdown
+
+*/
+
+ that_about . flag . main  =  1;
+
+
+
+/*
+
+ it is not get from hope
+
+*/
+
+ that_about . flag . deamon  =  gop_about . flag . deamon;
+
+
+ that_about . flag . dog  =  0;
+
+ that_about . flag . start  =  0;
+
+ that_about . flag . show  =  0;
+
+
+ that_about . flag . connector  =  0;
+
+ that_about . flag . connectted  =  0;
+
+
+ that_about . flag . sound  =  0;
+
+ that_about . flag . sound_show  =  0;
+
+ that_about . flag . sound_save  =  0;
+  
+  }
