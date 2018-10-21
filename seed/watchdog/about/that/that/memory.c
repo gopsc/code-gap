567b25836a345681013a45492c1ec5755ac3fd6d
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 16:05:44 +0800

    make it simple

---
+++ seed/watchdog/about/that/that/memory.c
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ struct gopi_memory
+
+  {
+
+
+ char symbol [ 16 ];
+
+
+
+ unsigned long  total;
+
+ unsigned long  free ;
+
+/*
+
+ we note rate straitly
+
+*/
+
+ unsigned long  rate;
+
+
+  };
