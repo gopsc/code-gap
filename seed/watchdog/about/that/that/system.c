92f8510dbd69b47c2bbad3dcdd2f20338feefbdc
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 16:03:47 +0800

    things about system

---
+++ seed/watchdog/about/that/that/system.c
@@ -0,0 +1,53 @@
+
+
+
+
+
+ struct gopi_system
+
+  {
+
+
+
+/*
+
+ use to put its type now could be wait or what
+
+
+ then it put about waitting could be little
+
+*/
+
+
+ char symbol [ 16 ];
+
+
+
+
+
+/*
+
+ 1024 is too long to save system name
+
+*/
+
+ char system [ 32 ];
+
+ char name   [ 32 ];
+
+/*
+
+ char user   [ 32 ];
+
+*/
+
+  };
+
+
+
+
+
+
+
+
+
