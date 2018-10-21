e3cfd27a313e4e562f2f0669a86e2f786208bd90
gopsc <35726593+gopsc@users.noreply.github.com>
2018-10-21 16:07:33 +0800

    make it simple

---
+++ seed/watchdog/about/that/that/network.c
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ struct gopi_network
+
+
+  {
+
+
+ char symbol [ 16 ];
+
+
+
+ /*
+
+  maybe use malloc straightly
+
+ */
+
+ char ip [ 32 ] [ 3 ] [ 1024 ];
+
+ int  number;
+
+
+  };
