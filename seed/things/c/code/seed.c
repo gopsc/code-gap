c704b4c728d91fd4705801875e256a9d6da08c31
gapsc <1776177662@qq.com>
2018-10-25 16:27:00 +0800

    make it simple and get seed

--- seed/things/c/code/seed.c
+++ seed/things/c/code/seed.c
@@ -1,5 +1,42 @@
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//#include <unistd.h>
+
+//#include <math.h>
+//#include <string.h>
+//#include <time.h>       // for time ( NULL )
+
+
+
+
+//#include <ifaddrs.h>
+//#include <fcntl.h>
+
+//#include <dirent.h>     // for dirent
+
+
+//#include <pthread.h>    // for thread
+
+
+//#include <signal.h>     // for check_deamon() ->
+                        // for tcp block out.
+
+//#include <sys/time.h>   // for note_save() -> gettimeofday()
+
+//#include <sys/socket.h> // for network -> socket
+
+#include <sys/stat.h>
+
+//#include <sys/types.h>  // for check_deamon() ->
+//#include <sys/param.h>  // for ...
+
+//#include <arpa/inet.h>  // for network
+
+
+
 
 
 
@@ -10,7 +47,7 @@
 /*
 
  make a seed
- 
+
  to get gop
 
 */
@@ -22,9 +59,11 @@ int main ()
   {
 
 
+/*
 
  load_hope ();
 
+*/
 
 
 
@@ -33,11 +72,158 @@ int main ()
 
 
 
+/*
 
  action_seed ( "base" );
 
 
  action_flower ( "base" );
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+ char * note_flower;
+
+ FILE * note_file;
+
+
+ char note_words [ 128 ];
+
+ int note_number;
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ check seed   <-
+
+ read seed
+
+ save flower
+
+*/
+
+
+ struct stat note_status;
+
+
+
+ if (
+
+      lstat ( "seed/seed", & note_status  ) != 0
+
+    )
+
+
+  {
+
+ printf ( "%s", "can't find seed \n" );
+
+ return -1;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ check seed
+
+ read seed   <-
+
+ get flower
+
+*/
+
+
+
+
+ note_flower = malloc ( 128 );
+
+ note_number = 128;
+
+
+
+
+
+ note_file  =  fopen ( "seed/seed", "r" );
+
+
+
+ while (
+
+         fgets ( note_words, 128, note_file ) != NULL
+
+       )
+
+  {
+
+ note_number += 128;
+
+ note_flower = realloc ( note_flower, note_number );
+
+ strcat ( note_flower, note_words );
+
+  }
+
+
+
+
+
+
+
+ printf ( "%s \n", note_flower );
+
+
+
+
+
+
+
+
 
 
 
@@ -46,7 +232,7 @@ int main ()
 
 
 
- return 1;
+ return 0;
 
 
   }
--- seed/things/c/system/file-system/file/is.c
+++ seed/things/c/system/file-system/file/is.c
@@ -1,11 +1,40 @@
 
-int is_file( const char *that_path ){
-
-    struct stat buffer_status;
 
 
-    if ( lstat(that_path, &buffer_status) == 0 ) {
-        return 1;}
 
-    else {
-        return 0;}}
+
+
+
+ int is_file ( const char * that_path )
+
+
+  {
+
+
+ struct stat note_status;
+
+
+ if (
+
+ lstat ( that_path, & note_status ) == 0
+
+    )
+
+  {
+
+ return 0;
+
+  }
+
+
+ else
+
+  {
+
+ return -1;
+
+  }
+
+
+
+  }
