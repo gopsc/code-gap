f24b89e3a1e7c104ab0d084998c029d4761341e6
gap <1776177662@qq.com>
2018-12-27 04:34:40 +0800

    is's about trees and dog

--- seed/dog/that/about/shadow.c
+++ seed/dog/that/about/shadow.c
@@ -16,14 +16,24 @@
 
 
 
+/*
+
  struct gopi_disk
 
+*/
+
+ struct that_shadow
+ 
   {
 
 
+/*
+
  char symbol [ 16 ];
 
+*/
 
+ void * symbol;
 
 
 
@@ -38,6 +48,37 @@
 
 */
 
- unsigned long  rate;
+ unsigned short  rate;
 
   };
+
+
+
+
+
+
+
+
+
+
+/*
+
+ int clean_about_disk ( gopi_disk that_disk )
+
+*/
+
+ int clean_about_shadow ( void * that_shadow )
+
+  {
+
+
+ * ( unsigned long )  that_shadow . size  =  0;
+
+ * ( unsigned long )  that_shadow . available  =  0;
+
+ * ( unsigned short ) that_shadow . rate  =  0;
+
+
+ return 0;
+
+  }
--- seed/dog/that/about/sister.c
+++ seed/dog/that/about/sister.c
@@ -2,25 +2,36 @@
 
 
 
+/*
 
  struct gopi_system
 
+*/
+
+
+ struct that_sister
+ 
   {
 
 
 
 /*
 
- use to put its type now could be wait or what
+ use to put its type now could be waitting or others
 
 
- then it put about waitting could be little
+ then put it about waitting could be little
 
 */
 
 
+/*
+
  char symbol [ 16 ];
 
+*/
+
+ void * symbol;
 
 
 
@@ -31,15 +42,28 @@
 
 */
 
+
+/*
+
  char system [ 32 ];
 
  char name   [ 32 ];
+
+*/
+
+
+ void *  sister;
+ 
+ void *  name;
+ 
 
 /*
 
  char user   [ 32 ];
 
 */
+
+
 
   };
 
@@ -51,3 +75,36 @@
 
 
 
+
+
+/*
+
+ int clean_about_sister ( gopi_sister that_sister )
+
+*/
+
+
+ int clean_about_sister ( void * that_sister )
+
+  {
+ 
+ 
+ 
+ 
+ strcpy ( that_sister . sister, "" );
+
+ strcpy ( that_sister . name, "" );
+
+
+/*
+
+ strcpy ( that_sister . user, "" );
+
+*/
+ 
+ 
+ 
+ 
+ return 0;
+ 
+  }
