296fea8b37fac9853d689e7cf800314292d5287d
gap <1776177662@qq.com>
2018-12-28 00:18:07 +0800

    sprinkle seed and add project

---
+++ seed/things/library_stm32.h
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+/*
+
+ the mcu of kylin mini board is  stm32f103rct6
+
+
+
+
+
+ this is the base head file
+
+ and don't know where it come
+
+*/
+
+#include "startup_stm32f10x_hd.s"
+
+#include "sister/sister_init.h"
+
+//#include <stm32f10x.h>
+
+
+
+
+
+// this is head files
+
+#include "things/varilables.h"
+
+#include "things/Kylin_mini_v1.2.h"
+
+
+
+
+// this is files for acrossing stm32 in the future
+
+#include "sister/time.h"
+
+#include "sister/gpio.h"
+
+
+
