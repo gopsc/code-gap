9df49962a75c92218af31d847612dd06f19537dc
Your Name <1776177662@qq.com>
2019-01-09 06:15:31 +0800

    good at least i add about fsst compiler what

---
+++ things/stm/seed/kylin-mini_version-1.2.c
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ the leds on kylin mini stm32 board version 1.2
+
+*/
+
+
+/*
+
+int group_led = GPIOB;
+
+int pin_led_1 = GPIO_Pin_6;
+
+int pin_led_2 = GPIO_Pin_7;
+
+int pin_led_3 = GPIO_Pin_8;
+
+int pin_led_4 = GPIO_Pin_9;
+
+*/
+
+
+/*
+
+ ŸoÖ· µÆ×éÌ– Õß
+
+
+ ŸoÖ· µÆ¼× Õß
+
+ ŸoÖ· µÆÒÒ Õß
+
+ ŸoÖ· µÆ±û Õß
+
+ ŸoÖ· µÆ¶¡ Õß
+
+*/
+
+
+ void  *  pin_group_led;
+
+
+ void  *  pin_led_one;
+
+ void  *  pin_led_two;
+
+ void  *  pin_led_three;
+
+ void  *  pin_led_four;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
