e2cd6048fe5853e915b5a48c1969041b9ae4f658
gopsc <1776177662@qq.com>
2019-01-23 03:10:08 +0800

    ^-^

---
+++ rain/hoby/brela.c
@@ -0,0 +1,114 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ void  *  starfall  (  void  *  sky  )
+
+  {
+
+*/
+
+
+ void  *  dream_saw_starfall   (  void  *  sky  )
+
+  {
+
+
+/*
+
+ 無形 星落 擇 無形 蒼天 者
+
+ 其
+
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+
+ if dream gose wrong
+
+ it will return null 
+
+*/
+
+
+ return   dream_wake  (  "yes"  );
+
+
+/*
+
+ 回應 夢醒 擇 「然」 者也
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+/*
+
+ 終也
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
