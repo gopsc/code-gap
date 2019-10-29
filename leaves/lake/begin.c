7649ca4bfe521c12488c01af1be5d81a0fa69b85
gopsc <35726593+gopsc@users.noreply.github.com>
2019-10-29 16:17:46 +0800

    ^-^

--- leaves/lake/begin.c
+++ leaves/lake/begin.c
@@ -13,9 +13,11 @@ void  *   begin_leaves_lake   ()
 
 that_leaves_lake_that  =  malloc  (  1 * sizeof ( void * )  );
 
-that_leaves_lake_that  [ 0 ]   =   malloc (  1 * sizeof ( int )  );
+that_leaves_lake_that  =  malloc  (  2 * sizeof ( void * )  );
+  
+that_leaves_lake_that  [ 0 ] [ 1 ]   =   malloc (  1 * sizeof ( int )  );
 
-*  (  int *  )  that_leaves_lake_that [ 0 ]  =  0;
+*  (  int *  )  that_leaves_lake_that [ 0 ] [ 1 ]  =  0;
 
 
 return NULL;
