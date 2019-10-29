f83f4073eb64040f0e75c126279f978a3a7cbb8f
gopsc <35726593+gopsc@users.noreply.github.com>
2019-10-29 13:57:21 +0800

    ^-^

--- leaves/lake/begin.c
+++ leaves/lake/begin.c
@@ -16,9 +16,15 @@ that_leaves_lake_that  =  malloc  (  1 * sizeof ( void * )  );
 that_leaves_lake_that  [ 0 ]   =   malloc (  1 * sizeof ( int )  );
 
 *  (  int *  )  that_leaves_lake_that [ 0 ]  =  0;
-  
+
+
+
+/*
+
 printf (  "%d\n",  * ( int * ) that_leaves_lake_that [ 0 ]  );
-  
+
+*/
+
 return NULL;
 
 }
