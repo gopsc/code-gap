7f9d3413f83eec306f0cba48082bfe7b993d9205
gap <1776177662@qq.com>
2019-10-30 16:34:43 +0800

    '-'

--- leaves/lake/fish.c
+++ leaves/lake/fish.c
@@ -15,8 +15,42 @@ void *  fish_fst_leaves_lake
 
 void *  note_number_count;
 
+note_number_count   =   malloc  (  1 * sizeof ( int )  );
 
 
+for
+
+(
+
+* ( int * ) note_number_count  =  1;
+
+* ( int * ) note_number_count  <=  * ( int * ) that_fst_leaves_lake  [ 0 ] [ 1 ];
+
+* ( int * ) note_number_count  =  * ( int * ) note_number_count   +   1
+
+)
+
+{
+
+
+if   (   strcmp  (  that_name,  that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 0 ]  )   ==  0   )
+
+{
+
+
+free  ( note_number_count );
+
+
+return   that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 1 ];
+
+}
+
+}
+
+
+
+
+free ( note_number_count );
 
 return  that_fst_leaves_lake;
 
