6f671d5311f53f1df1d03d0e34360fae4fdceafa
gap <1776177662@qq.com>
2019-10-30 21:45:27 +0800

    '-'

--- leaves/lake/end.c
+++ leaves/lake/end.c
@@ -12,9 +12,9 @@ void *  end_fst_leaves_lake ()
 {
 
 
-void *  note_number_count;
+void *  note_numbers_count;
 
-note_number_count   =   malloc  (  1 * sizeof ( int )  );
+note_numbers_count   =   malloc  (  1 * sizeof ( int )  );
 
 
 
@@ -23,25 +23,25 @@ for
 
 (
 
-* ( int * ) note_number_count  =  1;
+* ( int * ) note_numbers_count  =  1;
 
-* ( int * ) note_number_count  <=  * ( int * ) that_fst_leaves_lake  [ 0 ] [ 1 ];
+* ( int * ) note_numbers_count  <=  * ( int * ) that_fst_leaves_lake  [ 0 ] [ 1 ];
 
-* ( int * ) note_number_count  =  * ( int * ) note_number_count   +   1
+* ( int * ) note_numbers_count  =  * ( int * ) note_numbers_count   +   1
 
 )
 
 {
 
-free   (  that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 1 ]  );
+free   (  that_fst_leaves_lake  [  * ( int * ) note_numbers_count  ] [ 1 ]  );
 
-free   (  that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 0 ]  );
+free   (  that_fst_leaves_lake  [  * ( int * ) note_numbers_count  ] [ 0 ]  );
 
-free   (  that_fst_leaves_lake  [  * ( int * ) note_number_count  ]  );
+free   (  that_fst_leaves_lake  [  * ( int * ) note_numbers_count  ]  );
 
 }
 
-free  (  note_number_count );
+free  (  note_numbers_count );
 
 
 
--- leaves/lake/fish.c
+++ leaves/lake/fish.c
@@ -13,35 +13,41 @@ void *  fish_fst_leaves_lake
 {
  
 
-void *  note_number_count;
+void *  note_places_result;
 
-note_number_count   =   malloc  (  1 * sizeof ( int )  );
+
+void *  note_numbers_count;
+
+note_numbers_count   =   malloc  (  1 * sizeof ( int )  );
 
 
 for
 
 (
 
-* ( int * ) note_number_count  =  1;
+* ( int * ) note_numbers_count  =  1;
 
-* ( int * ) note_number_count  <=  * ( int * ) that_fst_leaves_lake  [ 0 ] [ 1 ];
+* ( int * ) note_numbers_count  <=  * ( int * ) that_fst_leaves_lake  [ 0 ] [ 1 ];
 
-* ( int * ) note_number_count  =  * ( int * ) note_number_count   +   1
+* ( int * ) note_numbers_count  =  * ( int * ) note_numbers_count   +   1
 
 )
 
 {
 
 
-if   (   strcmp  (  that_words_name,  that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 0 ]  )   ==  0   )
+if   (   strcmp  (  that_words_name,  that_fst_leaves_lake  [  * ( int * ) note_numbers_count  ] [ 0 ]  )   ==  0   )
 
 {
 
 
-free  ( note_number_count );
+note_places_result   =   that_fst_leaves_lake  [  * ( int * ) note_numbers_count  ] [ 1 ];
+
+free  (  note_numbers_count  );
 
 
-return   that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 1 ];
+return  note_places_result;
+
 
 }
 
@@ -50,7 +56,7 @@ return   that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 1 ];
 
 
 
-free ( note_number_count );
+free ( note_numbers_count );
 
 return  that_fst_leaves_lake;
 
