516b46d23d08872f38496b55ee5ef96f19f89ad3
gap <1776177662@qq.com>
2019-10-30 12:35:51 +0800

    '-'

--- leaves/lake/end.c
+++ leaves/lake/end.c
@@ -12,7 +12,50 @@ void *  end_fst_leaves_lake ()
 {
 
 
+void *  note_number_count;
 
-return  0;
+note_number_count   =   malloc  (  1 * sizeof ( int )  );
+
+
+
+
+for
+
+(
+
+* ( int * ) note_number_count  =  1;
+
+* ( int * ) note_number_count  <=  * ( int * ) that_fst_leaves_lake  [ 0 ] [ 1 ];
+
+* ( int * ) note_number_count  =  * ( int * ) note_number_count   +   1
+
+)
+
+{
+
+free   (  that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 1 ]  );
+
+free   (  that_fst_leaves_lake  [  * ( int * ) note_number_count  ] [ 0 ]  );
+
+free   (  that_fst_leaves_lake  [  * ( int * ) note_number_count  ]  );
+
+}
+
+
+
+
+free  (  that_fst_leaves_lake  [ 0 ] [ 1 ]  );
+
+free  (  that_fst_leaves_lake  [ 0 ] [ 0 ]  );
+
+free  (  that_fst_leaves_lake  [ 0 ]  );
+
+
+
+
+free  (  note_number_count );
+
+
+return  that_fst_leaves_lake;
 
 }
