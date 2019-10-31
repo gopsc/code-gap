5db53c443beb6377ee2018c112858dad5e678051
gopsc <35726593+gopsc@users.noreply.github.com>
2019-10-31 18:07:16 +0800

    ^-^

---
+++ things/linux/trees/files/save.c
@@ -0,0 +1,41 @@
+
+
+
+
+
+#include  <stdio.h>
+
+#include  <sys/stat.h>
+
+
+void *  save_fst_things_trees_files
+
+(
+
+void *  that_words_name,
+
+void *  that_words_content
+
+)
+
+{
+
+void  *   note_files_save  =  fopen  (  that_words_name,  "w"  );
+
+
+fprintf  (  note_files_save,  "%s",  that_words_content  );
+
+
+fclose  (  note_files_save  );
+
+
+
+return  0;
+
+}
+
+
+
+
+
+
