7d646aa60353232875f9d6c23c4c2c8aed827889
gopsc <35726593+gopsc@users.noreply.github.com>
2019-10-31 18:02:33 +0800

    ^-^

--- things/linux/trees/files/append.c
+++ things/linux/trees/files/append.c
@@ -10,12 +10,20 @@ void *  append_fst_things_trees_files
 
 void *  that_words_name,
 
-void *  that_words_convent
+void *  that_words_content
 
 )
 
 {
 
+
+FILE *  note_files_append  =  fopen ( that_words_name, "a" );
+
+
+fprintf  (  note_files_append,  "%s",  that_words_content  );
+
+
+fclose ( note_files_append );
 
 
 
