6a5884679dd7f2a484698f9a3e999082df754d2c
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-01 14:37:50 +0800

    ^-^

--- leaves/system/trees/dirents/is.c
+++ leaves/system/trees/dirents/is.c
@@ -3,9 +3,6 @@
 
 
 
-#include  <stdio.h>
-
-#include  <sys/stat.h>
 
 
 
@@ -18,94 +15,27 @@ void  *   is_fst_leaves_system_trees_dirents
 
 
 
-
-
-
-
-
-
-
-
-
-
-void  *   note_struct_status;
-
-
-note_struct_status  =  malloc  (  sizeof ( struct stat )  );
-
-
-
-
-
-
-
-
-
-
-if  (    lstat (  that_words_name,  note_struct_status  )  !=  0    )
-
-{
-
-free ( note_struct_status );
-
-return   fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_no"  );
-
-}
-
-
-
-
-
-
-else
+   
+if  (  is_fst_things_trees_dirent  (  that_words_name  )  ==  1  )
    
 {
-   
-
-   
-   
-   
-   
-   
-   
-   
-   
-   
-if  (    S_ISDIR  (        ( * ( struct stat * ) note_struct_status )  . st_mode       )  ==  0    )
-
-
-
-{
-
-free ( note_struct_status );
 
 return  fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_no"  );
-
+   
+   
 }
 
 
+ 
+else if  (  is_fst_things_trees_dirent  (  that_words_name  )  ==  0  )
    
-   
-
-else
-
 {
 
-free ( note_struct_status );
-
 return  fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_yes"  );
-
+   
+   
 }
 
-
-
-
-
-
-
-
-
-}
 
 
 
