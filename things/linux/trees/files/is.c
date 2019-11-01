89dd55d4172c6d5aa6349cc6a06a84417c16852b
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-01 14:54:26 +0800

    ^-^

---
+++ things/linux/trees/files/is.c
@@ -0,0 +1,98 @@
+
+
+
+
+
+
+#include  <stdio.h>
+
+#include  <sys/stat.h>
+
+
+
+
+void *  is_things_trees_files 
+
+(  void *  that_words_name  )
+
+
+
+{
+
+
+
+
+void  *   note_struct_status;
+
+
+note_struct_status  =  malloc  (  sizeof ( struct stat )  );
+
+
+
+
+
+
+if  (    lstat  ( that_words_name, note_struct_status )  !=  0    )
+
+{
+
+free ( note_struct_status );
+
+return 1;
+
+}
+
+
+    
+    
+
+else
+
+{
+
+
+
+
+
+
+
+
+
+
+if  (    S_ISREG  (    (  * ( struct stat * )  note_struct_status  )  . st_mode     )  ==  0    )
+
+{
+
+free ( note_struct_status );
+
+return  1;
+
+}
+
+    
+    
+    
+else
+    
+{
+
+free ( note_struct_status );
+
+return  0;
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
