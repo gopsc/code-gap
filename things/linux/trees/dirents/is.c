f322c8fb9cb535c2a551c61372b2b7c302b9c23e
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-01 14:34:12 +0800

    ^-^

---
+++ things/linux/trees/dirents/is.c
@@ -0,0 +1,109 @@
+
+
+#include  <stdio.h>
+
+#include  <sys/stat.h>
+
+
+
+
+void  *   is_fst_things_trees_dirents
+
+
+(  void *  that_words_name  )
+
+
+
+{
+
+
+
+
+
+
+
+
+void  *   note_struct_status;
+
+
+note_struct_status  =  malloc  (  sizeof ( struct stat )  );
+
+
+
+
+
+
+
+
+
+
+if  (    lstat (  that_words_name,  note_struct_status  )  !=  0    )
+
+{
+
+free ( note_struct_status );
+
+return   1;
+
+}
+
+
+
+
+
+
+else
+   
+{
+   
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+if  (    S_ISDIR  (        ( * ( struct stat * ) note_struct_status )  . st_mode       )  ==  0    )
+
+
+
+{
+
+free ( note_struct_status );
+
+return  1;
+
+}
+
+
+   
+   
+
+else
+
+{
+
+free ( note_struct_status );
+
+return  0;
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
