990883ee74c74e037573f5f31c967a541bff665b
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-01 14:39:33 +0800

    ^-^

--- fst.c
+++ fst.c
@@ -128,72 +128,6 @@ void fft(int site) {
 
 
 
-void test_ft(const char* site) {
-
-
-
-
-
-
-
-    double t;
-
-    double n;
-
-
-
-    for( n=1;n<=4096;n=n+1 ) {
-
-
-
-        t=n/4096;
-
-        result_sound[1][(int)n] = sin(2*M_PI*160*t+1)*10;}
-
-
-
-
-
-
-
-
-
-    if      ( strcmp(site, "snake") == 0 ) {
-
-
-
-        output_print("string", "FT test start.\n");
-
-
-
-        dft(1);
-
-
-
-        output_print("string", "FT test done.\n");}
-
-
-
-
-
-
-
-
-
-    else if ( strcmp(site, "fft") == 0 ) {
-
-
-
-        output_print("string", "FT test start.\n");
-
-
-
-        fft(1);
-
-
-
-        output_print("string", "FT test done.\n");}}
-
 */
 
 
@@ -203,6 +137,8 @@ void test_ft(const char* site) {
 
 
 #include  "./things/linux/trees/dirents/create.c"
+
+#include  "./things/linux/trees/dirents/is.c"
 
 #include  "./things/linux/trees/files/append.c"
 
