d3b306d36b39e310893b2fb354275eff821b71c7
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-01 16:52:30 +0800

    ^-^

--- leaves/system/trees/files/get.c
+++ leaves/system/trees/files/get.c
@@ -35,6 +35,9 @@ if  (  is_fst_leaves_system_trees_files  (  that_words_name  )  ==  fish_fst_lea
 read_fst_things_trees_files  (  that_words_name,  fish_fst_leaves_lake ( that_words_name )  );
 
 
+printf (  "%s\n", fish_fst_leaves_lake ( that_words_name )  );
+
+
 return  fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_yes"  );
 
 
