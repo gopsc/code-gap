de04a46be9ea26b7c1389dab6808eafde7c02239
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-01 16:17:14 +0800

    ^-^

---
+++ things/linux/trees/files/read.c
@@ -0,0 +1,121 @@
+
+
+
+#include  <stdio.h>
+
+#include  <sys/stat.h>
+
+
+
+
+void *  read_fst_things_trees_files
+
+(  
+
+void *  that_words_name,
+
+void *  that_words_result
+
+)
+
+
+
+{
+
+
+
+
+
+void  *   note_files_get;
+
+
+that_words_result  =  malloc  (  1  *  sizeof ( char )  );
+
+* ( char * ) that_words_result  =  '\0';
+
+
+
+
+
+
+
+void  *   note_words_read;
+
+
+note_words_read  =  malloc  (  2  *  sizeof ( char )  );
+
+
+
+
+
+
+
+void *  note_numbers_count;
+
+
+note_numbers_count  =  malloc  (  1  *  sizeof ( int )  );
+
+
+
+
+
+
+
+
+
+
+note_files_get   =   fopen  (  that_words_name,  "r"  );
+
+
+
+
+
+while  (    fgets  (   note_words_read,   2,   ( FILE * ) note_files_get   )   !=   NULL    )
+
+{
+
+
+
+for
+
+(
+
+* ( int * ) note_numbers_count   =   0;
+
+*  (  that_words_result  +  * ( int * ) note_numbers_count  )   !=   '\0';
+
+* ( int * ) note_numbers_count   =   * ( int * ) note_numbers_count   +   1
+
+)
+
+{
+
+}
+
+
+
+that_words_result  =  realloc (  that_words_result,   ( 1  +  1  +  strlen ( that_words_result ) )  *  sizeof ( char )  );
+
+
+*   (   that_words_result   +   * ( int * ) note_numbers_count   )   =   * note_words_read；
+
+*   (   that_words_result   +   * ( int * ) note_numbers_count   +   1   )   =   '\0';
+
+
+
+}
+
+
+
+
+
+
+fclose  (  note_files_get  );
+
+free  (  note_words_read  );
+
+free  (  note_numbers_count  );
+
+return 0;
+
+}
