2d61c45d94a3ebd9e0acff25c0564d2902f3b2b4
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-13 17:13:55 +0800

    ^_^

--- things/data/words/measure.c
+++ things/data/words/measure.c
@@ -7,9 +7,9 @@ void *  measure_fst_things_data_words
 
 (
 
-void *  that_words_target,
+void *  that_words_targets,
 
-void *  that_numbers_result
+void *  that_numbers_results
 
 )
 
@@ -31,7 +31,7 @@ for
 
 * ( int * ) note_numbers_counts   =   0;
 
-* ( char * ) ( that_words_target  +  * ( int * ) note_numbers_counts )   !=   '\0';
+* ( char * ) ( that_words_targets  +  * ( int * ) note_numbers_counts )   !=   '\0';
 
 * ( int * ) note_numbers_counts   =   * ( int * ) note_numbers_counts   +   1
 
@@ -44,7 +44,7 @@ for
 
 
 
-* ( int * ) that_numbers_result   =   * ( int * ) note_numbers_counts;
+* ( int * ) that_numbers_results   =   * ( int * ) note_numbers_counts;
 
 
 free  (  note_numbers_counts  );
