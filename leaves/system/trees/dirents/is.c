a56d0f80391dfb293efac1630c6c257fae81b854
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-13 17:15:07 +0800

    ^-^

--- leaves/system/trees/dirents/is.c
+++ leaves/system/trees/dirents/is.c
@@ -8,30 +8,30 @@
 
 void  *   is_fst_leaves_system_trees_dirents
 
-(  void  *   that_words_name  )
+(  void  *   that_words_names  )
 
 {
 
 
 
-void *  note_numbers_result;
+void *  note_numbers_results;
 
-note_numbers_result   =   create_fst_things_data  (  1 * sizeof ( int )  );
+note_numbers_results   =   create_fst_things_data  (  1 * sizeof ( int )  );
 
    
    
    
    
    
-is_fst_things_trees_dirents  (  that_words_name,  note_numbers_result  );
+is_fst_things_trees_dirents  (  that_words_names,  note_numbers_results  );
 
 
-if  (  * ( int * ) note_numbers_result  ==  0  )
+if  (  * ( int * ) note_numbers_results  ==  0  )
    
 {
 
 
-free  ( note_numbers_result  );
+free  ( note_numbers_results  );
    
 return  fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_yes"  );
    
@@ -42,7 +42,7 @@ return  fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_yes"  );
 
 
 
-free  ( note_numbers_result  );
+free  ( note_numbers_results  );
 
 return  fish_fst_leaves_lake  (  "that_fst_leaves_lake_existence_no"  );
 
