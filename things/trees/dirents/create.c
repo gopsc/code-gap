525db87a332a1631b110ee1d2f615d08ef1acda2
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-13 16:57:55 +0800

    ^-^

--- things/trees/dirents/create.c
+++ things/trees/dirents/create.c
@@ -11,12 +11,12 @@
 
 void *  create_fst_things_trees_dirents
 
-( void *  that_words_name )
+( void *  that_words_names )
 
 {
 
 
-mkdir  (  that_words_name,  S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH   );
+mkdir  (  that_words_names,  S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH   );
 
 
 return 0;
