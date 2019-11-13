3110bcd52f7f364a33037479c9beda6cc3a88634
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-13 17:18:47 +0800

    ^-^

--- leaves/system/trees/files/save.c
+++ leaves/system/trees/files/save.c
@@ -1,8 +1,6 @@
 
 
 
-#include  <stdlib.h>
-
 
 
 
@@ -10,9 +8,9 @@ void  *   save_fst_leaves_system_trees_files
 
 (
 
-void  *   that_words_name,
+void  *   that_words_names,
    
-void  *   that_words_content
+void  *   that_words_contents
 
 )
                      
@@ -24,29 +22,29 @@ void  *   that_words_content
 
 
 
-void  *   note_numbers_count;
+void  *   note_numbers_counts;
 
-note_numbers_count  =  create_fst_things_data  (  1  *  sizeof ( int )    );
+note_numbers_counts  =  create_fst_things_data  (  1  *  sizeof ( int )    );
 
-* ( int * ) note_numbers_count  =  1;
+* ( int * ) note_numbers_counts  =  1;
 
 
 
-void *  note_numbers_count_two;
+void *  note_numbers_counts_two;
 
-note_numbers_count_two  =  create_fst_things_data  (  1  *  sizeof ( int )  );   
+note_numbers_counts_two  =  create_fst_things_data  (  1  *  sizeof ( int )  );   
    
    
    
    
 
-void  *   note_words_name;
+void  *   note_words_names;
 
-note_words_name  =  create_fst_things_data  (    strlen ( that_words_name )  *  sizeof ( char )    );
+note_words_names  =  create_fst_things_data  (    strlen ( that_words_names )  *  sizeof ( char )    );
 
 
 
-void  *   note_place_name;
+void  *   note_place_names;
 
 
 
@@ -73,7 +71,7 @@ note_words_name       /...
 
 
 
-while  (    * ( int * ) note_numbers_count  !=  0    )
+while  (    * ( int * ) note_numbers_counts  !=  0    )
 
 {
 
@@ -85,9 +83,9 @@ while  (    * ( int * ) note_numbers_count  !=  0    )
 
 
 
-copy_fst_things_data_words  (  note_words_name,  that_words_name  );
+copy_fst_things_data_words  (  note_words_names,  that_words_names  );
     
-note_place_name  =  note_words_name;
+note_place_names  =  note_words_names;
 
 
 
@@ -103,11 +101,11 @@ for
 
 (
 
-* ( int * ) note_numbers_count_two   =   0;
+* ( int * ) note_numbers_counts_two   =   0;
 
-* ( int * ) note_numbers_count_two   <   * ( int * ) note_numbers_count;
+* ( int * ) note_numbers_counts_two   <   * ( int * ) note_numbers_counts;
 
-* ( int * ) note_numbers_count_two   =   * ( int * ) note_numbers_count_two   +   1  )
+* ( int * ) note_numbers_counts_two   =   * ( int * ) note_numbers_counts_two   +   1  )
 
 {
 
@@ -117,22 +115,22 @@ find_fst_things_data_words
    
 (
 
-note_place_name + 1,
+note_places_name + 1,
    
 "/",
    
-note_place_name
+note_places_name
 
 );
 
 
 
 
-if  (  note_place_name   ==   NULL  )
+if  (  note_places_name   ==   NULL  )
 
 {
 
-* ( int * ) note_numbers_count   =   0;
+* ( int * ) note_numbers_counts   =   0;
 
 }
 
@@ -148,30 +146,30 @@ if  (  note_place_name   ==   NULL  )
 
 
 
-if (  * ( int * ) note_numbers_count   !=   0  )
+if (  * ( int * ) note_numbers_counts   !=   0  )
 
 {
 
 
    
-* ( int * ) note_numbers_count   =   * ( int * ) note_numbers_count   +   1;
+* ( int * ) note_numbers_counts   =   * ( int * ) note_numbers_counts   +   1;
 
 
 
-* ( char * ) note_place_name  =  '\0';
+* ( char * ) note_places_names  =  '\0';
 
 
 
 
    
-is_fst_things_trees_dirents  (  note_words_name,  note_numbers_results  );
+is_fst_things_trees_dirents  (  note_words_names,  note_numbers_results  );
    
    
 if  (  * ( int * ) note_numbers_results   ==   1  )
 
 {
 
-create_fst_things_trees_dirents  (  note_words_name  );
+create_fst_things_trees_dirents  (  note_words_names  );
 
 }
 
@@ -192,13 +190,11 @@ create_fst_things_trees_dirents  (  note_words_name  );
 
 
 
-free_fst_things_data  (  note_words_name  );
+free_fst_things_data  (  note_words_names  );
 
-free_fst_things_data  (  note_numbers_count  );
+free_fst_things_data  (  note_numbers_counts  );
 
-free_fst_things_data  (  note_numbers_count_two  );
-
-
+free_fst_things_data  (  note_numbers_counts_two  );
 
 
 
@@ -207,7 +203,9 @@ free_fst_things_data  (  note_numbers_count_two  );
 
 
 
-is_fst_things_trees_dirents  (  that_words_name,  note_numbers_results  );
+
+
+is_fst_things_trees_dirents  (  that_words_names,  note_numbers_results  );
 
 
 if  (  * ( int * ) note_numbers_results   ==   1  )
@@ -215,7 +213,7 @@ if  (  * ( int * ) note_numbers_results   ==   1  )
 {
 
 
-write_fst_things_trees_files  (  that_words_name,  that_words_content  );
+write_fst_things_trees_files  (  that_words_names,  that_words_contents  );
 
 
    
