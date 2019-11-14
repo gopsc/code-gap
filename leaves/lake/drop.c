c5d775c649f3ac3399e6892b33fbd6a7b39fe1dd
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-14 15:26:07 +0800

    ^-^

--- leaves/lake/drop.c
+++ leaves/lake/drop.c
@@ -10,7 +10,7 @@
 
 void *  drop_fst_leaves_lake
 
-(  void *  that_words_name  )
+(  void *  that_words_names  )
 
 {
 
@@ -21,19 +21,19 @@ void *  drop_fst_leaves_lake
 
 
 
-void *  note_numbers_count;
+void *  note_numbers_counts;
 
 
-note_numbers_count   =   create_fst_things_data  (  1  *  sizeof ( int )  );
+note_numbers_counts   =   create_fst_things_data  (  1  *  sizeof ( int )  );
 
 
 
 
-void *  note_numbers_switch;
+void *  note_numbers_switches;
 
-note_numbers_switch   =  create_fst_things_data  (  1  *  sizeof ( int )  );
+note_numbers_switches   =  create_fst_things_data  (  1  *  sizeof ( int )  );
 
-* ( int * ) note_numbers_switch   =   0;
+* ( int * ) note_numbers_switches   =   0;
 
 
 
@@ -44,11 +44,11 @@ for
 
 (
 
-* ( int * ) note_numbers_count   =   1;
+* ( int * ) note_numbers_counts   =   1;
 
-* ( int * ) note_numbers_count  <=   * ( int * ) that_fst_leaves_lake [ 0 ] [ 1 ];
+* ( int * ) note_numbers_counts  <=   * ( int * ) that_fst_leaves_lake [ 0 ] [ 1 ];
 
-* ( int * ) note_numbers_count   =   * ( int * ) note_numbers_count   +   1
+* ( int * ) note_numbers_counts   =   * ( int * ) note_numbers_counts   +   1
 
 )
 
@@ -60,26 +60,26 @@ for
 
 
 
-if  (  * ( int * ) note_numbers_switch   ==   0  )
+if  (  * ( int * ) note_numbers_switches   ==   0  )
 
 {
 
 
 
 
-if  (   strcmp  (  that_words_name,  that_fst_leaves_lake  [ * ( int * ) note_numbers_count ] [ 0 ]  )   ==   0   )
+if  (   strcmp  (  that_words_names,  that_fst_leaves_lake  [ * ( int * ) note_numbers_counts ] [ 0 ]  )   ==   0   )
 
 {
 
 
-free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_count ] [ 0 ]  );
+free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_counts ] [ 0 ]  );
 
-free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_count ] [ 1 ]  );
+free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_counts ] [ 1 ]  );
 
-free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_count ]  );
+free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_counts ]  );
 
 
-* ( int * ) note_numbers_switch  =  1;
+* ( int * ) note_numbers_switches  =  1;
 
 
 }
@@ -90,11 +90,11 @@ free  (  that_fst_leaves_lake  [ * ( int * ) note_numbers_count ]  );
 
 
 
-else if  (  * ( int * ) note_numbers_switch  ==  1  )
+else if  (  * ( int * ) note_numbers_switches  ==  1  )
 
 {
 
-that_fst_leaves_lake [ * ( int * ) note_numbers_count  -  1 ]   =   that_fst_leaves_lake  [ * ( int * ) note_numbers_count ];
+that_fst_leaves_lake [ * ( int * ) note_numbers_counts  -  1 ]   =   that_fst_leaves_lake  [ * ( int * ) note_numbers_counts ];
 
 }
 
@@ -109,7 +109,7 @@ that_fst_leaves_lake [ * ( int * ) note_numbers_count  -  1 ]   =   that_fst_lea
   
   
 
-if  (  * ( int * ) note_numbers_switch   ==   1  )
+if  (  * ( int * ) note_numbers_switches   ==   1  )
 
 {
 
@@ -126,9 +126,9 @@ that_fst_leaves_lake   =   realloc  (  that_fst_leaves_lake,  ( 1  +  * ( int * 
 
 
 
-free  (  note_numbers_count  );
+free  (  note_numbers_counts  );
 
-free  (  note_numbers_switch );
+free  (  note_numbers_switches );
 
 return  that_fst_leaves_lake;
 
