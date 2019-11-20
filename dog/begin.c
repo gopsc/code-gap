90a551d2791abea4c3ff910160447cbe74d53f6e
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-20 17:59:10 +0800

    ^-^

--- dog/begin.c
+++ dog/begin.c
@@ -10,33 +10,33 @@ void  *   begin_fst_dog ()
 
 {
 
-write_fst_leaves_lake  (  "fst,dog,main",  "no"  );
+write_fst_leaves_lake  (  "that,fst,dog,main",  "no"  );
 
-write_fst_leaves_lake  (  "fst,dog,circle",  "no"  );
-
-
-write_fst_leaves_lake  (  "fst,dog",  "no"  );
-
-write_fst_leaves_lake  (  "fst,dog,start",  "no"  );
-
-write_fst_leaves_lake  (  "fst,dog,show",  "no"  );
+write_fst_leaves_lake  (  "that,fst,dog,circle",  "no"  );
 
 
-write_fst_leaves_lake  (  "fst,dog,female",  "no"  );
+write_fst_leaves_lake  (  "that,fst,dog",  "no"  );
 
-write_fst_leaves_lake  (  "fst,dog,male",  "no"  );
+write_fst_leaves_lake  (  "that,fst,dog,start",  "no"  );
 
-
-write_fst_leaves_lake  (  "fst,dog,voice",  "no"  );
-
-write_fst_leaves_lake  (  "fst,dog,show",  "no"  );
+write_fst_leaves_lake  (  "that,fst,dog,show",  "no"  );
 
 
-write_fst_leaves_lake  (  "fst,dog,symbol",  "waitting"  );
+write_fst_leaves_lake  (  "that,fst,dog,female",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,male",  "no"  );
+
+
+write_fst_leaves_lake  (  "that,fst,dog,voice",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,show",  "no"  );
+
+
+write_fst_leaves_lake  (  "that,fst,dog,symbol",  "waitting"  );
 
 
 
-return 0;
+return   fish_fst_leaves_lake  (  "that,fst,leaves,lake,existence,yes"  );
 
 }
 
