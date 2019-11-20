1ceebf9236d9756eac1acc49a604bb20cab4d08f
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-20 17:58:12 +0800

    ^-^

--- leaves/lake/existence/end.c
+++ leaves/lake/existence/end.c
@@ -14,12 +14,12 @@ void  *   end_fst_leaves_lake_existence  ()
 
 
 
-drop_fst_leaves_lake  (  "that_fst_leaves_lake_existence_yes"  );
+drop_fst_leaves_lake  (  "that,fst,leaves,lake,existence,yes"  );
 
-drop_fst_leaves_lake  (  "that_fst_leaves_lake_existence_no"  );
+drop_fst_leaves_lake  (  "that,fst,leaves,lake,existence,no"  );
 
 
 
-return   that_fst_leaves_lake;
+return   0;
 
 }
