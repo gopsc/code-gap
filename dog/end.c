03fc640c1b2b604121836797da361feb283f04e7
gopsc <35726593+gopsc@users.noreply.github.com>
2019-11-20 18:04:42 +0800

    ^-^

--- dog/end.c
+++ dog/end.c
@@ -10,29 +10,29 @@ void  *   end_fst_dog ()
 
 {
 
-drop_fst_leaves_lake  (  "fst,dog,main"  );
+drop_fst_leaves_lake  (  "that,fst,dog,main"  );
 
-drop_fst_leaves_lake  (  "fst,dog,circle"  );
-
-
-drop_fst_leaves_lake  (  "fst,dog"  );
-
-drop_fst_leaves_lake  (  "fst,dog,start"  );
-
-drop_fst_leaves_lake  (  "fst,dog,show"  );
+drop_fst_leaves_lake  (  "that,fst,dog,circle"  );
 
 
-drop_fst_leaves_lake  (  "fst,dog,female"  );
+drop_fst_leaves_lake  (  "that,fst,dog"  );
 
-drop_fst_leaves_lake  (  "fst,dog,male"  );
+drop_fst_leaves_lake  (  "that,fst,dog,start"  );
 
-
-drop_fst_leaves_lake  (  "fst,dog,voice"  );
-
-drop_fst_leaves_lake  (  "fst,dog,show"  );
+drop_fst_leaves_lake  (  "that,fst,dog,show"  );
 
 
-drop_fst_leaves_lake  (  "fst,dog,symbol"  );
+drop_fst_leaves_lake  (  "that,fst,dog,female"  );
+
+drop_fst_leaves_lake  (  "that,fst,dog,male"  );
+
+
+drop_fst_leaves_lake  (  "that,fst,dog,voice"  );
+
+drop_fst_leaves_lake  (  "that,fst,dog,show"  );
+
+
+drop_fst_leaves_lake  (  "that,fst,dog,symbol"  );
 
 
 
