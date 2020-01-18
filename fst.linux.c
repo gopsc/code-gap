5bb3f6c1a0aa22ab4cfe70c30168b51a9d50d5bf
gopsc <35726593+gopsc@users.noreply.github.com>
2020-01-18 16:42:51 +0800

    ^-^

---
+++ fst.linux.c
@@ -0,0 +1,89 @@
+
+
+
+#include "./root/data/create.c"
+
+#include "./root/data/resize.c"
+
+#include "./root/data/free.c"
+
+
+#include "./root/data/words/find.c"
+/*
+#include "./things/data/words/copy.c"
+
+#include "./things/data/words/append.c"
+
+#include "./things/data/words/measure.c"
+
+#include "./things/data/words/compare.c"
+
+
+#include "./things/trees/dirents/create.c"
+
+#include "./things/trees/dirents/is.c"
+
+
+#include "./things/trees/files/is.c"
+
+#include "./things/trees/files/append.c"
+
+#include "./things/trees/files/write.c"
+
+#include "./things/trees/files/read.c"
+
+
+#include "./leaves/lake/that.c"
+
+#include "./leaves/lake/begin.c"
+
+#include "./leaves/lake/end.c"
+
+
+#include "./leaves/lake/write.c"
+
+#include "./leaves/lake/fish.c"
+
+#include "./leaves/lake/drop.c"
+   
+
+
+#include "./leaves/lake/existence/begin.c"
+
+#include "./leaves/lake/existence/end.c"
+
+
+
+#include "./leaves/system/trees/dirents/is.c"
+
+
+#include "./leaves/system/trees/files/is.c"
+
+#include "./leaves/system/trees/files/save.c"
+
+#include "./leaves/system/trees/files/append.c"
+
+#include "./leaves/system/trees/files/get.c"
+
+
+
+#include "./dog/begin.c"
+
+#include "./dog/end.c"
+
+
+#include "./spider/that/hope.h"
+
+#include "./spider/that/that/that.h"
+
+#include "./spider/that/lake/that.h"
+
+
+#include "./voice/that/hope.h"
+
+#include "./voice/that/that.h"
+
+*/
+
+#include "./leaves/that/seed.c"
+
