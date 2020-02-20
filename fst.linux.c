ef73ea0eea2ece509de194e05928e7c6af7d6dce
gap <1776177662@qq.con>
2020-02-21 04:27:42 +0800

    checked and modified "roots/lake/create.computer" by changing variables names rule

--- fst.linux.c
+++ fst.linux.c
@@ -57,10 +57,10 @@
 
 
 #include "./roots/lakes/bubbles/prepare.computer"
-//
+
 
 #include "./roots/lakes/create.computer"
-
+//
 #include "./roots/lakes/resize.computer"
 
 #include "./roots/lakes/free.computer"
