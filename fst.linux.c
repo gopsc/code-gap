1f478bc1c37ff238570d17f29b708dfa4e1bcf22
gap <1776177662@qq.con>
2020-03-06 15:08:38 +0800

    modified "fst/roots/lakes/stones/append.computer" by changing variables names rule

--- fst.linux.c
+++ fst.linux.c
@@ -67,9 +67,9 @@
 
 
 #include "./roots/lakes/stones/copy.computer"
-//
-#include "./roots/lakes/stones/append.computer"
 
+#include "./roots/lakes/stones/append.computer"
+//
 #include "./roots/lakes/stones/measure.computer"
 
 #include "./roots/lakes/stones/compare.computer"
