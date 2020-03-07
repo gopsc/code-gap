808fa2d97928a0d93783da9fa2dc70352d3ae2d9
gap <1776177662@qq.con>
2020-03-07 18:41:39 +0800

    checked and modified "fst/roots/trees/files/write.computer" by changing variables' name rule and lines' rule

--- fst.linux.c
+++ fst.linux.c
@@ -98,9 +98,9 @@ gcc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiring
 
 
 #include "./roots/trees/files/is.linux"
-//
-#include "./roots/trees/files/write.computer"
 
+#include "./roots/trees/files/write.computer"
+//
 #include "./roots/trees/files/read.computer"
 
 
