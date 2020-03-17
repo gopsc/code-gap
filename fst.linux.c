8d9c026ccbfe8a3e39a3f8683f43da74cffae389
gap <1776177662@qq.con>
2020-03-18 07:53:42 +0800

    checked and modified "fst/branches/trees/dirents/remove.linux" by changing blank lines rule and changing function and variables names

--- fst.linux.c
+++ fst.linux.c
@@ -167,13 +167,13 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 #include "./branches/trees/dirents/is.linux"
 
 #include "./branches/trees/dirents/create.linux" // _uint8_t and numbers may changed
-//
+
 #include "./branches/trees/dirents/remove.linux"
 
-
+//
 #include "./branches/trees/files/is.linux"
 
-#include "./branches/trees/files/save.linux"  //  __uint8_t
+#include "./branches/trees/files/save.linux"  //  __uint8_t and numbers may changed
 
 #include "./branches/trees/files/get.linux"
 
