18af74eff233615e5adddef45b4a7d5602ecf72c
gap <1776177662@qq.com>
2020-03-18 08:18:10 +0800

    added a note to "fst/fst.linux.c" and modified "fst/branches/flowers/seed.linux" by changing type of function returning

--- fst.linux.c
+++ fst.linux.c
@@ -183,7 +183,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 
 
-#include "./branches/flowers/seed.linux"
+#include "./branches/flowers/seed.linux" // int straightly
 
 
 
