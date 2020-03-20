35d7582f1674371fbd69b68115189a9524a27931
gap <1776177662@qq.con>
2020-03-21 07:23:51 +0800

    checked, tested and modified "fst/roots/lakes/waves/*" by changing including to notes

--- fst.linux.c
+++ fst.linux.c
@@ -99,7 +99,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 
 #include "./roots/lakes/prepare.computer"
-//
+
 
 #include "./roots/lakes/waves/create.computer"
 
@@ -107,7 +107,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./roots/lakes/waves/free.computer"
 
-
+//
 #include "./roots/lakes/stones/copy.computer"
 
 #include "./roots/lakes/stones/append.computer"
