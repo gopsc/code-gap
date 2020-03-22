aef396c0c76c44462ccc2d10faae67c2c2276394
gap <1776177662@qq.con>
2020-03-22 12:33:25 +0800

    modified "fst/roots/lakes/stones/compare.computer", ".../find.computer" and ".../measure.computer" by exchanging "waters" and "silts"

--- fst.linux.c
+++ fst.linux.c
@@ -54,7 +54,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include <math.h>
                                // for pi
-                               //  for trigonometric function
+                               // for trigonometric function
 
 
 
@@ -163,14 +163,14 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 #include "./roots/lakes/stones/copy.computer"
 
 #include "./roots/lakes/stones/append.computer"
-//
+
 #include "./roots/lakes/stones/measure.computer"
 
 #include "./roots/lakes/stones/compare.computer"
 
 #include "./roots/lakes/stones/find.computer"
 
-
+//
 
 #include "./roots/trees/remove.computer"
 
