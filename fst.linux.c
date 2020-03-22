1a0fd5309dc6d2c290f399f4c18c2a7b8d98a8ac
gap <1776177662@qq.com>
2020-03-22 18:44:07 +0800

    added notes to "fst/fst.linux.c"

--- fst.linux.c
+++ fst.linux.c
@@ -182,9 +182,9 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./roots/trees/files/is.linux" // note_structure
 
-#include "./roots/trees/files/write.computer"
+#include "./roots/trees/files/write.computer" // note_files
 
-#include "./roots/trees/files/read.computer"
+#include "./roots/trees/files/read.computer" // note_files
 
 //
 
