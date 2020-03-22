f4dba8cad7ac052d12938aa131630f1f6dba8c07
gap <1776177662@qq.con>
2020-03-22 12:13:22 +0800

    modified "fst/roots/lakes/waves/create.computer" by using new type modified "fst/roots/threads/*", "fst/roots/lakes/waves/*" and "fst/roots/lakes/stones/copy.computer", "fst/roots/lakes/stones/append.computer" by exchanging "waters" and "silts" in variables

--- fst.linux.c
+++ fst.linux.c
@@ -140,7 +140,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 #include "./branches/intros.computer"
 
 
-//
+
 
 #include "./roots/threads/prepare.computer"
 
@@ -163,7 +163,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 #include "./roots/lakes/stones/copy.computer"
 
 #include "./roots/lakes/stones/append.computer"
-
+//
 #include "./roots/lakes/stones/measure.computer"
 
 #include "./roots/lakes/stones/compare.computer"
