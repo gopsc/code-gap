66c9e927a9a3d046bb7f57ae1298401313eaab44
gap <1776177662@qq.con>
2020-03-24 07:58:17 +0800

    added including in "fst/roots/threads/prepare.computer" modified "fst/roots/lakes/prepare.computer" and "fst/roots/lakes/waves/*" by changing blank lines rule and using type defied before

--- fst.linux.c
+++ fst.linux.c
@@ -132,7 +132,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 
 #include "./roots/threads/prepare.computer"  // unassigned variable
-//
+
 #include "./roots/threads/begin.computer"
 
 #include "./roots/threads/end.computer"
@@ -148,7 +148,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./roots/lakes/waves/free.computer"
 
-
+//
 #include "./roots/lakes/stones/copy.computer"
 
 #include "./roots/lakes/stones/append.computer"
