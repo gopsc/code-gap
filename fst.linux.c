7de50d99f886cfd59bf9feef9cd42ce576e14337
gap <1776177662@qq.con>
2020-03-24 07:39:10 +0800

    started "fst/skies/winds/fst.clouds"

--- fst.linux.c
+++ fst.linux.c
@@ -132,7 +132,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 
 #include "./roots/threads/prepare.computer"  // unassigned variable
-
+//
 #include "./roots/threads/begin.computer"
 
 #include "./roots/threads/end.computer"
