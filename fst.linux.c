b8cf4fc9ebea83521597056b20bb3f4c4e1f8a88
gap <1776177662@qq.con>
2020-03-26 06:26:09 +0800

    added "fst/branches/intros.computer" checked and modified "fst/branches/lakes/waves/write.computer", ".../drop.computer" and ".../fish.computer" by changing lines rule and changing names of type

--- fst.linux.c
+++ fst.linux.c
@@ -193,14 +193,14 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./branches/lakes/end.computer"
 
-//
-#include "./branches/lakes/waves/write.computer" // numbers may changed
+
+#include "./branches/lakes/waves/write.computer"
 
 #include "./branches/lakes/waves/drop.computer" // numbers may changed
 
 #include "./branches/lakes/waves/fish.computer"
 
-
+//
 
 #include "./branches/lakes/bubbles/begin.computer" // ( down here ) use but don't need to include
 
