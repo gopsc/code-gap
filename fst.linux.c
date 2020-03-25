870a8e75dc7a8fcdb694a107771e448de7ed6014
gap <1776177662@qq.con>
2020-03-25 08:03:47 +0800

    modified "fst/roots/lakes/stones/append.computer" by changing lines rule and changing names of type

--- fst.linux.c
+++ fst.linux.c
@@ -150,9 +150,9 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 
 #include "./roots/lakes/stones/copy.computer"
-//
-#include "./roots/lakes/stones/append.computer"
 
+#include "./roots/lakes/stones/append.computer"
+//
 #include "./roots/lakes/stones/measure.computer"
 
 #include "./roots/lakes/stones/compare.computer"
