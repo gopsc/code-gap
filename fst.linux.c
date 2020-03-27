896ac42354ab8a49b7ef8a37dc27f84c61e2cdb9
gap <1776177662@qq.con>
2020-03-28 04:56:54 +0800

    modified "fst/roots/trees/*" by changing types fuction income modified "fst/branches/trees/*" by changing types funtion takes

--- fst.linux.c
+++ fst.linux.c
@@ -160,7 +160,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./roots/lakes/stones/find.computer"
 
-//
+
 
 #include "./roots/trees/remove.computer"
 
