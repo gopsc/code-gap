6d530502f1ab7cc525832af27e8c2ccd1ac70133
gap <1776177662@qq.con>
2020-03-31 07:34:16 +0800

    checked "fst/roots/threads/*" and "fst/roots/lakes/*"

--- fst.linux.c
+++ fst.linux.c
@@ -133,7 +133,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 
 #include "./roots/threads/prepare.computer"
-//
+
 #include "./roots/threads/begin.computer"
 
 #include "./roots/threads/end.computer"
@@ -160,7 +160,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./roots/lakes/stones/find.computer"
 
-
+//
 
 #include "./roots/trees/remove.computer"
 
