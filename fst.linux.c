b3f66a4f0115ec26422dba97bb069f9f2ac6abfe
gap <1776177662@qq.con>
2020-03-30 13:12:24 +0800

    changed "fst/branches/intros.computer"

--- fst.linux.c
+++ fst.linux.c
@@ -129,11 +129,11 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./branches/intros.computer"
 
-//
+
 
 
 #include "./roots/threads/prepare.computer"
-
+//
 #include "./roots/threads/begin.computer"
 
 #include "./roots/threads/end.computer"
