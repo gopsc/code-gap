2d21499bc4b866e14fab626e706b7f1781fa1801
gap <1776177662@qq.con>
2020-04-15 03:33:49 +0800

    checked and modified "fst/roots/trees/*" by changing program order and blank lines rule

--- fst.linuxes.c
+++ fst.linuxes.c
@@ -160,7 +160,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 
 #include "./roots/lakes/stones/find.bases"
 
-//
+
 
 #include "./roots/trees/remove.computers"
 
@@ -177,7 +177,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   -lm   -lpthead   (-lasound)   (-lwiringp
 #include "./roots/trees/files/read.computers" // note_files; can't return failed; may pre-memory small
 
 
-
+//
 
 #include "./branches/threads/prepare.computers"
 
