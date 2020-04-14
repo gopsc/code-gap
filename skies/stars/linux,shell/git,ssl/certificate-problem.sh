034295df9d16ea1e1152a5fd50316a4ae2a91ea8
gap <1776177662@qq.con>
2020-04-14 22:53:24 +0800

    modified files in "fst/skies/stars"

---
+++ skies/stars/linux,shell/git,ssl/certificate-problem.sh
@@ -0,0 +1,3 @@
+# ssl certificate problem: unable to get local issuer certificate
+
+git config --global http.sslVerify false
---
+++ skies/stars/linux,shell/git,ssl/error.sh
@@ -0,0 +1,3 @@
+# open ssl ssl_connect: ssl_error_syscall in connection to ... :443
+
+git config --global --unset http.proxy
