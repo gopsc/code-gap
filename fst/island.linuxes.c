2e4e3c9fc5847614a5ca5886543047994cf6d7d2
gapsc <1776177662@qq.com>
2020-06-12 12:55:18 +0800

    added "fst/branches/lakes/waves/graze.bases" finished "fst/skies/winds/fst,branches,lakes,waves,graze.rains"

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -256,6 +256,8 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 
 #include "./branches/lakes/waves/fish.bases"
 
+#include "./branches/lakes/waves/graze.bases"
+
 
 #include "./branches/lakes/bubbles/begin.bases"
 
