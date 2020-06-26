16c3de0f3c2f9dec50813300da0c015c361fdec9
gapsc <1776177662@qq.com>
2020-06-27 02:18:46 +0800

    checked and modified "fst/branches/threads/*" and "fst/branches/lakes/begin.bases" to remove some processes in transforms

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -241,7 +241,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 
 #include "./branches/threads/forms/waters/deeper.bases"
 
-
+//
 
 #include "./branches/lakes/prepare.bases"
 
@@ -249,7 +249,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 
 #include "./branches/lakes/end.bases"
 
-//
+
 #include "./branches/lakes/waves/add.bases"
 
 #include "./branches/lakes/waves/drop.bases"
