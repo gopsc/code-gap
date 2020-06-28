f991493186e51867a35ff4bc85872a09e4f24702
gapsc <1776177662@qq.com>
2020-06-29 03:09:31 +0800

    checked and modified "fst/branches/lakes/end.bases" to change some processes in transforms and change lines rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -246,9 +246,9 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 #include "./branches/lakes/prepare.bases"
 
 #include "./branches/lakes/begin.bases"
-//
-#include "./branches/lakes/end.bases"
 
+#include "./branches/lakes/end.bases"
+//
 
 #include "./branches/lakes/waves/add.bases"
 
