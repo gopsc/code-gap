0e23d6a995a05cdaf52b39fa1c1358c2cb031b52
gapsc <1776177662@qq.com>
2020-07-04 04:36:24 +0800

    checked and modified "fst/branches/lakes/waves/add.bases" to add blank pointer return and change lines rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -263,9 +263,9 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 
 #include "./branches/lakes/end.bases"
 
-//
-#include "./branches/lakes/waves/add.bases"  // support types include waters, silts and stones
 
+#include "./branches/lakes/waves/add.bases"  // support types include waters, silts and stones
+//
 #include "./branches/lakes/waves/drop.bases"
 
 #include "./branches/lakes/waves/fish.bases"
