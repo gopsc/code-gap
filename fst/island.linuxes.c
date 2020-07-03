72d883a43bf9b6ca1e6c7c91c3cb89e812a24e79
gapsc <1776177662@qq.com>
2020-07-03 19:29:58 +0800

    modified "fst/branches/lakes/bubbles/begin.bases" to change lines rule modified "fst/branches/lakes/bubbles/moons/begin.bases" to add new type blank pointer return

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -277,7 +277,7 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 
 #include "./branches/lakes/bubbles/end.bases"
 
-
+//
 #include "./branches/lakes/bubbles/moons/begin.bases"
 
 #include "./branches/lakes/bubbles/moons/end.bases"
