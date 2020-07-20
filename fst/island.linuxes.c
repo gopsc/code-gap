54d630cd5126bac23eed40b09ac41c5cf41434c8
gapsc <1776177662@qq.com>
2020-07-21 06:16:16 +0800

    checked and modified "fst/branches/trees/dirents/create.linuxes" to change orders of processes checked and modified "fst/branches/trees/files/save.linuxes" to add blank results return, add type transforms and lines rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -320,9 +320,9 @@ cc   /opt/TxL/flower.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwir
 
 
 #include "./branches/trees/files/is.linuxes"
-//
-#include "./branches/trees/files/save.linuxes"
 
+#include "./branches/trees/files/save.linuxes"
+//
 #include "./branches/trees/files/get.linuxes"
 
 #include "./branches/trees/files/remove.linuxes"
