6e80371c886a957fbd2e8371b437db8a2eddd125
gapsc <1776177662@qq.com>
2020-07-29 19:25:27 +0800

    checked and modified "fst/roots/lakes/stones/append.bases" to change lines rule and chenge blank charactors rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -203,9 +203,9 @@
 
 
 #include "./roots/lakes/stones/copy.bases"
-//
-#include "./roots/lakes/stones/append.bases"
 
+#include "./roots/lakes/stones/append.bases"
+//
 #include "./roots/lakes/stones/measure.bases"
 
 #include "./roots/lakes/stones/compare.bases"
