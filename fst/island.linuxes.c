41f5549b530b960b10e028015fd9bf0889573e9b
gapsc <1776177662@qq.com>
2020-08-08 06:33:14 +0800

    ^-^

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -260,9 +260,9 @@
 
 #include "./branches/lakes/end.bases"
 
-//
-#include "./branches/lakes/waves/add.bases"  // support types include waters, silts and stones; can't add null
 
+#include "./branches/lakes/waves/add.bases"  // support types include waters, silts and stones; can't add null
+//
 #include "./branches/lakes/waves/drop.bases"
 
 #include "./branches/lakes/waves/fish.bases"
