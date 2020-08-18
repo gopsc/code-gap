3730edb449e61417fe2196c52cec419160ec80b1
gapsc <1776177662@qq.com>
2020-08-19 03:26:05 +0800

    checked and modified "fst/roots/threads/end.bases" to add type transforms and add blank charactors

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -194,10 +194,10 @@
 #include "./roots/threads/prepare.bases"
 
 #include "./roots/threads/begin.bases"
-//
+
 #include "./roots/threads/end.bases"
 
-
+//
 
 #include "./roots/lakes/prepare.bases"
 
