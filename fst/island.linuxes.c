e9f04c9e537a359c3afcd2dd6997c58444020bee
gapsc <1776177662@qq.com>
2020-08-19 23:02:35 +0800

    checked and modified "fst/roots/lakes/stones/append.bases" to add type transforms, add blank charactors and change lines rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -213,9 +213,9 @@
 
 
 #include "./roots/lakes/stones/copy.bases"
-//
-#include "./roots/lakes/stones/append.bases"
 
+#include "./roots/lakes/stones/append.bases"
+//
 #include "./roots/lakes/stones/measure.bases"
 
 #include "./roots/lakes/stones/compare.bases"
