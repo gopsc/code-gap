6124477f710a1ab607d1e9910711b1d3ff2e5496
gapsc <1776177662@qq.com>
2020-08-19 09:12:29 +0800

    ^-^

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -211,9 +211,9 @@
 
 #include "./roots/lakes/waters/clean.bases"
 
-//
-#include "./roots/lakes/stones/copy.bases"
 
+#include "./roots/lakes/stones/copy.bases"
+//
 #include "./roots/lakes/stones/append.bases"
 
 #include "./roots/lakes/stones/measure.bases"
