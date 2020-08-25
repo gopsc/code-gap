b75e5cc8ed7e63fa130ced9c52e8df2db352ccf0
gapsc <1776177662@qq.com>
2020-08-26 07:42:10 +0800

    checked and modified "fst/branches/lakes/begin.bases" to add type transforms and change lines rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -256,9 +256,9 @@
 
 
 #include "./branches/lakes/prepare.bases"
-//
-#include "./branches/lakes/begin.bases"
 
+#include "./branches/lakes/begin.bases"
+//
 #include "./branches/lakes/end.bases"
 
 
