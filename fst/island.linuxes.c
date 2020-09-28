a6d3e1b4f9b28d4b3d4fa1fa08fed2aecf3204f4
gapsc <1776177662@qq.com>
2020-09-28 16:07:02 +0800

    checked and modified "fst/branches/lakes/waves/drop.bases" to change lines rule

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -272,9 +272,9 @@
 
 
 #include "./branches/lakes/waves/add.bases"  // support types include waters, silts and stones
-//
-#include "./branches/lakes/waves/drop.bases"
 
+#include "./branches/lakes/waves/drop.bases"
+//
 #include "./branches/lakes/waves/fish.bases"
 
 #include "./branches/lakes/waves/graze.bases"
