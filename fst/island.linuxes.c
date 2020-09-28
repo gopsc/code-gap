9695bd82213be5548f588ec3dc0783df2e9e2c74
gapsc <1776177662@qq.com>
2020-09-29 04:55:34 +0800

    checked "fst/branches/lakes/bubbles/end.bases"

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -281,10 +281,10 @@
 
 
 #include "./branches/lakes/bubbles/begin.bases"
-//
+
 #include "./branches/lakes/bubbles/end.bases"
 
-
+//
 #include "./branches/lakes/bubbles/moons/begin.bases"
 
 #include "./branches/lakes/bubbles/moons/end.bases"
