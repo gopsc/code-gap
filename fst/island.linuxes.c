4840ffd05fb13bfa9df5b94f368a5f9b4ee2edc1
gapsc <1776177662@qq.com>
2020-10-05 05:49:54 +0800

    ^-^wq

--- fst/island.linuxes.c
+++ fst/island.linuxes.c
@@ -95,10 +95,10 @@
 
 
 #include "./branches/lakes/bubbles/moons/begin.bases"
-//
+
 #include "./branches/lakes/bubbles/moons/end.bases"
 
-
+//
 #include "./branches/lakes/bubbles/lights/begin.bases"
 
 #include "./branches/lakes/bubbles/lights/end.bases"
