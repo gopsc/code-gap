d735a8e9b999d0a8ea6bf158b7e0db3fe8532ddb
gap <1776177662@qq.con>
2020-10-30 07:27:30 +0800

    checked and modified "./fst/compile.sh" to change wrong charactors

--- fst/compile.sh
+++ fst/compile.sh
@@ -1,7 +1,6 @@
 
 
-# cc ./fst/island.c (-std=c++0x) (-lm) (-lpthead) (-lasound) (-lwiringpi) -o ?
+# cc   ./fst/island.linuxes.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwiringpi)   -o ?
 
 cc ./fst/island.c -o ?
-
 
