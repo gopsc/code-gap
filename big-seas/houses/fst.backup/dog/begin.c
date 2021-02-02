64783cd25842a1d8399bf2a521ed87968b139bb7
gapfc <1776177662@qq.com>
2021-02-02 20:32:41 +0000

    moved "./bigseas" to "./big-seas"

---
+++ big-seas/houses/fst.backup/dog/begin.c
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+void  *   begin_fst_dog ()
+
+{
+
+write_fst_leaves_lake  (  "that,fst,dog,main",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,circle",  "no"  );
+
+
+write_fst_leaves_lake  (  "that,fst,dog",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,start",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,show",  "no"  );
+
+
+write_fst_leaves_lake  (  "that,fst,dog,female",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,male",  "no"  );
+
+
+write_fst_leaves_lake  (  "that,fst,dog,voice",  "no"  );
+
+write_fst_leaves_lake  (  "that,fst,dog,show",  "no"  );
+
+
+write_fst_leaves_lake  (  "that,fst,dog,symbol",  "waitting"  );
+
+
+
+return   fish_fst_leaves_lake  (  "that,fst,leaves,lake,existence,yes"  );
+
+}
+
+
+
+
+
+
+
---
+++ big-seas/houses/fst.backup/spider/that/hope.h
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct   hope_spider
+
+
+{
+
+
+
+
+
+
+
+
+
+
+
+void  *   address;
+
+
+void  *   port_to;
+
+
+void  *   port_this;
+
+
+
+
+
+
+void  *   symbol_next;
+
+
+void  *   step_connection;
+
+
+void  *   size_update;
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
