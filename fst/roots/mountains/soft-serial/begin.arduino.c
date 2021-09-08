a22a6dd880d63edfe64c6a1e8c885b4dbe66b323
gopsc <35726593+gopsc@users.noreply.github.com>
2021-09-08 16:09:11 +0800

    ^-^

--- fst/roots/mountains/soft-serial/begin.arduino.c
+++ fst/roots/mountains/soft-serial/begin.arduino.c
@@ -1,4 +1,26 @@
 
 
+#ifdef that_circuits___poems-third
+
+#ifdef that_circuits___arduino
+
+
+#ifndef added___fst_roots__mountains_soft-serial___prepare
+
+#define added___fst_roots__mountains_soft-serial___prepare
+
+
+
+
 #include <SoftwareSerial.h>
+
+
+
+
+#endif
+
+
+#endif
+
+#endif
 
