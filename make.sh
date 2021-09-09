d027a21b51514abde514cdd94303d5e437184ba9
gopsc <35726593+gopsc@users.noreply.github.com>
2021-09-09 18:51:24 +0800

    ^-^

---
+++ make.sh
@@ -0,0 +1,16 @@
+
+
+#    ./fst/prepare.sh
+
+#    ./fst/package.sh
+
+
+# cc ./island.c  (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwiringpi)   -o ?
+
+# cc ./island.c        -o ./island
+
+  cc ./island.linux.c  -o  ./island
+
+
+# rm ./island.c
+
