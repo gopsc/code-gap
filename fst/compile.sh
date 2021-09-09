e8e2c2176020e8cbf3a3a6c268dd04a2915696bd
gopsc <35726593+gopsc@users.noreply.github.com>
2021-09-09 13:56:02 +0800

    ^-^

--- fst/compile.sh
+++ fst/compile.sh
@@ -1,12 +1,13 @@
 
 
+#    ./fst/prepare.sh
+
 #    ./fst/package.sh
 
 
-# cc ./fst/island.c   (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwiringpi)   -o ?
+# cc ./island.c  (-std=c++0x)   (-lm)   (-lpthead)   (-lasound)   (-lwiringpi)   -o ?
 
+# cc ./island.c   -o ./island
 
-  cc ./fst/island.linux.c
-
-# cc ./fst/island.c -o ./island
+  cc ./fst/island.linux.c   -o ./island
 
