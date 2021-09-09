e73e8aebe70445e205dd9217fcfe47e5186b83a8
gopsc <35726593+gopsc@users.noreply.github.com>
2021-09-09 14:05:35 +0800

    ^-^

---
+++ fst/prepare.sh
@@ -0,0 +1,19 @@
+
+
+echo  ''                                >   './island.c'
+
+
+echo  ''                                >>  './island.c'
+
+echo  '#define that_circuits___island'  >>  './island.c'
+
+
+echo  ''                                >>  './island.c'
+
+echo  '#define that_circuits___test'    >>  './island.c'
+
+
+echo  ''                                >>  './island.c'
+
+echo  ''                                >>  './island.c'
+
