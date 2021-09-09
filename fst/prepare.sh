3e7fde1691639e72be384fa80bb73e091cb4390b
gopsc <35726593+gopsc@users.noreply.github.com>
2021-09-09 17:34:40 +0800

    ^-^

--- fst/prepare.sh
+++ fst/prepare.sh
@@ -10,10 +10,33 @@ echo  '#define that_circuits___island'  >>  './island.c'
 
 echo  ''                                >>  './island.c'
 
-echo  '#define that_circuits___test'    >>  './island.c'
+#echo '#define that_circuits___test'    >>  './island.c'
 
 
 echo  ''                                >>  './island.c'
 
 echo  ''                                >>  './island.c'
+
+
+
+
+if [  $#  -gt  3  ];then
+
+
+    echo  '#define that_circuits_mountains___$1'  >>  './island.c'
+
+    echo  ''                                      >>  './island.c'
+
+
+    echo  '#define that_circuits_star-fall___$2'  >>  './island.c'
+
+    echo  ''                                      >>  './island.c'
+
+
+    echo  ''                                      >>  './island.c'
+
+    echo  ''                                      >>  './island.c'
+
+
+fi
 
