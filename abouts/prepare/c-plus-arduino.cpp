370c3c103cc06426d7f2ccf5e7904f7c712df6a3
gap <1776177662@qq.com>
2021-09-13 04:42:58 +0800

    ^-^

---
+++ abouts/prepare/c-plus-arduino.cpp
@@ -0,0 +1,33 @@
+
+
+#ifdef that_circuits___test
+
+
+#endif
+
+
+
+
+#ifdef that_circuits___island
+
+#ifdef that_circuits_starfall___c_plus_arduino
+
+
+
+
+#define that_circuits_starfall___poems_third_common
+
+
+#define that_circuits_starfall___poems_annotation_common
+
+#define that_circuits_starfall___poems_annotation_arduino
+
+
+
+
+#endif
+
+
+#endif
+
+
