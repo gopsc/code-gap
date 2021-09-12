cdbf5044c4ffdb29b1f488a017ee041ed176053c
gap <1776177662@qq.com>
2021-09-13 00:09:49 +0800

    ^-^

---
+++ abouts/prepare/c_plus_unix.cpp
@@ -0,0 +1,37 @@
+
+
+#ifdef that_circuits___test
+
+
+#endif
+
+
+
+
+#ifdef that_circuits___island
+
+#ifdef that_circuits_starfall___c_plus_unix
+
+
+
+
+#define that_circuits_starfall___poems_third_common
+
+#define that_circuits_starfall___poems_third_origin
+
+#define that_circuits_starfall___poems_third_unix
+
+
+#define that_circuits_starfall___poems_annotation_common
+
+#define that_circuits_starfall___poems_annotation_unix
+
+
+
+
+#endif
+
+
+#endif
+
+
