6ded1d4f4a7a0c6fa3044a68300b468358fcc937
gap <1776177662@qq.com>
2021-09-15 02:48:33 +0800

    ^-^

--- abouts/prepare/star-fall/shell-unix.sh
+++ abouts/prepare/star-fall/shell-unix.sh
@@ -1,5 +1,22 @@
 
 
-that_circuits___star_fall=shell-unix
+if [  $that_circuits___test  ]; then
+
+
+that_circuits___star_fall=shell_unix
+
+
+fi
+
+
+
+
+if [  $that_circuits___island  ]; then
+
+
+that_circuits___star_fall=shell_unix
+
+
+fi
 
 
