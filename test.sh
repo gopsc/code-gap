6c9f28ab825db11071b5e4a02844261b41e85d11
gap <1776177662@qq.com>
2021-09-15 04:38:43 +0800

    ^-^

--- test.sh
+++ test.sh
@@ -2,7 +2,24 @@
 dos2unix ./?.shell
 
 
+
+
+if [  $1  =  'c'  ]; then
+
 ./?.shell  island  android_termux c_scripts
+
+fi
+
+
+
+
+if [  $1  =  'shell'  ]; then
+
+./?.shell  island  android_termux shell_unix
+
+fi
+
+
 
 
 rm ./?
